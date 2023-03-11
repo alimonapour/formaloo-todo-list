8311cd169b4980f449323fcfdd441b931fb255e9
alimonapour <a.monapoor@gmail.com>
2023-03-12 01:42:25 +0330

    feat: add components directly to app and remove main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { Main } from './components/Main'
+import { Header } from './components/Header'
+import { GroupsTabsPanels } from './components/TasksContainer'
 
 function App() {
   return (
     <div className='App'>
-      <Main />
+      <div className='container w-[90%] lg:w-[60%] md:w-[80%] mx-auto my-2 p-5 border border-blue-600 rounded-md bg-white shadow-md shadow-gray-600 flex items-center flex-col'>
+        <Header />
+        <GroupsTabsPanels />
+      </div>
     </div>
   )
 }
