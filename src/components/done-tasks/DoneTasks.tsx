0d93cc03f626720400fa0e04cb70ff9bdf999064
alimonapour <a.monapoor@gmail.com>
2023-03-11 18:42:34 +0330

    feat: add change status to done function

--- src/components/done-tasks/DoneTasks.tsx
+++ src/components/done-tasks/DoneTasks.tsx
@@ -1,5 +1,25 @@
-import React from 'react'
+import { TaskType } from '../../types/TaskType'
 
-export const DoneTasks = () => {
-  return <div>DoneTasks</div>
+interface PropsType {
+  todo: TaskType
+  pendingTask(taskIdToPending: string): void
+}
+export const DoneTasks = ({ todo, pendingTask }: PropsType) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let isChecked = event.target.checked
+
+    if (isChecked === false) return pendingTask(todo.id)
+  }
+
+  return (
+    <div className='w-full flex border border-blue-400 p-2 my-2 rounded-md  justify-between'>
+      <div className='flex '>
+        <input type='checkbox' defaultChecked={true} onChange={handleChange} />
+        <div className='flex flex-col ml-4'>
+          <h1>{todo.title}</h1>
+          <h2>{todo.creationDate}</h2>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/components/todo-task/TodoTask.tsx
+++ src/components/todo-task/TodoTask.tsx
@@ -4,16 +4,21 @@ import { TaskType } from '../../types/TaskType'
 interface PropsType {
   todo: TaskType
   removeTask(taskIdToDelete: string): void
+  doneTask(taskIdToDone: string): void
 }
 
-export const TodoTask = ({ todo, removeTask }: PropsType) => {
+export const TodoTask = ({ todo, removeTask, doneTask }: PropsType) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let isChecked = event.target.checked
+    if (isChecked) return doneTask(todo.id)
+  }
   return (
     <div className='w-full flex border p-2 my-2 rounded-md border-blue-400 justify-between'>
       <div className='flex'>
-        <input type='checkbox' />
+        <input type='checkbox' onChange={handleChange} />
         <div className='flex flex-col ml-4'>
-          <h1>{todo.title}</h1>
-          <h2>{todo.creationDate}</h2>
+          <h1 className='text-md font-medium leading-5'>{todo.title}</h1>
+          <h2 className='text-sm font-medium leading-5'>{todo.creationDate}</h2>
         </div>
       </div>
       <div className='flex items-center'>
